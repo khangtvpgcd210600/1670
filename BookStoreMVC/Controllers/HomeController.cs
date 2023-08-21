fc119673da5d0c564929b7c25cd63aa902cb1800
khangtvpgcd210600 <khangtvpgcd210600@fpt.edu.vn>
2023-08-21 12:31:36 +0700

    add action into view part 1

--- BookStoreMVC/Controllers/HomeController.cs
+++ BookStoreMVC/Controllers/HomeController.cs
@@ -1,13 +1,12 @@
 ﻿using BookStoreMVC.Data;
 using BookStoreMVC.Models;
 using Microsoft.AspNetCore.Mvc;
-using Microsoft.EntityFrameworkCore;
 using System.Diagnostics;
 
 namespace BookStoreMVC.Controllers
 {
-	public class HomeController : Controller
-	{
+    public class HomeController : Controller
+    {
 		private readonly BookStoreMVCContext _context;
 
 		public HomeController(BookStoreMVCContext context)
@@ -16,47 +15,43 @@ namespace BookStoreMVC.Controllers
 		}
 
 		public IActionResult Index()
-		{
-			return View(_context.Book.ToList());
-		}
+        {
+            return View(_context.Book.ToList());
+        }
 
-		public IActionResult Privacy()
-		{
-			return View();
-		}
+        public IActionResult Privacy()
+        {
+            return View();
+        }
 
-		[ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
-		public IActionResult Error()
-		{
-			return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
-		}
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
 
-		public async Task<IActionResult> Search(string query)
-		{
-			var books = await _context.Book
-				.Where(b => b.Name.Contains(query))
-				.ToListAsync();
-			if (query == null)
-			{
-				books = await _context.Book
-				.ToListAsync();
-				return View("Index", books);
-			}
-			return View("Index", books);
-		}
-		public async Task<IActionResult> Filter(int genre)
-		{
+public async Task<IActionResult> Search(string query)
+        {
+            if (string.IsNullOrEmpty(query))
+            {
+                return View("Index", _context.Book.ToList());
+            }
+            var books = await _context.Book
+                .Where(b => b.Name.Contains(query))
+                .ToListAsync();
 
-			var book = await _context.Book
-				.Where(a => a.GenreId == genre)
-				.ToListAsync();
-			if (genre == 0)
-			{
-				book = await _context.Book
-				.ToListAsync();
-				return View("Index", book);
-			}
-			return View("Index", book);
-		}
-	}
+            return View("Index", books);
+        }
+        public async Task<IActionResult> Filter(int genre)
+        {
+            if (genre == 0)
+            {
+                return View("Index", _context.Book.ToList());
+            }
+            var book = await _context.Book
+                .Where(a => a.GenreId == genre)
+                .ToListAsync();
+            return View("Index", book);
+        }
+    }
 }
